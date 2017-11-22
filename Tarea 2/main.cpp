4eb4624f98b03549f31938cf3d5b6c2f8b500079
nachotp <thenachotp@gmail.com>
2017-11-21 22:52:03 -0300

    Lectura básica de input

--- Tarea 2/main.cpp
+++ Tarea 2/main.cpp
@@ -5,8 +5,20 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
+  int numEdificios, L, H, R, numDrones, X, Y;
+  ios::sync_with_stdio(false);
 
+  cin >> numEdificios;
+  for (size_t i = 0; i < numEdificios; ++i) {
+    cin >> L >> H >> R;
+  }
 
+  // hacer magia con los edificios
 
+  cin >> numDrones;
+  for (size_t i = 0; i < numDrones; ++i) {
+    cin >> X >> Y;
+    // consultar drones
+  }
   return 0;
 }
